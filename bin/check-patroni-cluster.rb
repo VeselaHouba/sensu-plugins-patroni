cca4df52d9ad68726b88850f6f13c8e88d79e3b2
Michalek <michalek@m-cloud.cz>
2021-09-28 15:45:35 +0200

    Fixes

--- bin/check-patroni-cluster.rb
+++ bin/check-patroni-cluster.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: false
+
 #
 # DESCRIPTION:
 #   Takes host + port combination and checks cluster status from patroni's
@@ -37,39 +38,39 @@ class CheckPatroni < Sensu::Plugin::Check::CLI
   option :url, short: '-u URL'
   option :host, short: '-h HOST', default: 'localhost'
   option :port, short: '-P PORT', proc: proc(&:to_i), default: 8008
-  option :lagwarn, long: '--lag-warn LAG', default: 1024
-  option :lagcrit, long: '--lag-crit LAG', default: 10240
+  option :lagwarn, long: '--lag-warn LAG', default: 1_024
+  option :lagcrit, long: '--lag-crit LAG', default: 10_240
   option :timeout, short: '-t SECS', proc: proc(&:to_i), default: 15
 
   def run
-    cluster = get_data("/cluster")
+    cluster = get_data('/cluster')
     # puts cluster
     crit = 0
     warn = 0
-    message = stats = ""
+    message = stats = ''
 
     # loop through members to get leader timeline
     ltimeline = get_leader_timeline(cluster)
 
     cluster['members'].each do |member|
-      stats += member['name']+", "+member['role']+", tl: "+member['timeline'].to_s
+      stats += member['name'] + ', ' + member['role'] + ', tl: ' + member['timeline'].to_s
       case member['role']
-      when "replica"
+      when 'replica'
         if member['lag'] > config[:lagwarn]
-          stats += "Lag for "+member['name']+" over "+config[:lagwarn].to_s+"\n"
+          stats += 'Lag for ' + member['name'] + ' over ' + config[:lagwarn].to_s + '\n'
           warn += 1
         end
         if member['lag'] > config[:lagcrit]
-          message += "Lag for "+member['name']+" over "+config[:lagcrit].to_s+"\n"
+          message += 'Lag for ' + member['name'] + ' over ' + config[:lagcrit].to_s + '\n'
           crit += 1
         end
         if member['timeline'] != ltimeline
-          message += "Timeline for "+member['name']+" does not match leader\n"
+          message += 'Timeline for ' + member['name'] + ' does not match leader\n'
           crit += 1
         end
-        stats +=", lag: "+member['lag'].to_s+"\n"
-      when "leader"
-        stats +="\n"
+        stats += ', lag: ' + member['lag'].to_s + '\n'
+      when 'leader'
+        stats += '\n'
       end
     end
     critical message + stats if crit > 0
@@ -81,19 +82,17 @@ class CheckPatroni < Sensu::Plugin::Check::CLI
     cluster['members'].each do |member|
       return member['timeline'] if member['role'] == 'leader'
     end
-    critical "Cluster has no leader"
+    critical 'Cluster has no leader'
   end
 
   def get_data(resource)
-    begin
-      Timeout.timeout(config[:timeout]) do
-        return acquire_resource(resource)
-      end
-    rescue Timeout::Error
-      critical 'Connection timed out'
-    rescue StandardError => e
-      critical "Connection error: #{e.message}"
+    Timeout.timeout(config[:timeout]) do
+      return acquire_resource(resource)
     end
+  rescue Timeout::Error
+    critical 'Connection timed out'
+  rescue StandardError => e
+    critical "Connection error: #{e.message}"
   end
 
   def json_valid?(str)
@@ -110,7 +109,6 @@ class CheckPatroni < Sensu::Plugin::Check::CLI
 
     critical 'invalid JSON from request' unless json_valid?(res.body)
 
-    json = ::JSON.parse(res.body)
-    return json
+    ::JSON.parse(res.body)
   end
 end
--- sensu-plugins-patroni.gemspec
+++ sensu-plugins-patroni.gemspec
@@ -22,31 +22,26 @@ Gem::Specification.new do |s| # rubocop:disable Metrics/BlockLength
                                'release_prerelease' => 'false' }
   s.name                   = 'sensu-plugins-patroni'
   s.platform               = Gem::Platform::RUBY
-  s.post_install_message   = 'You can use the embedded Ruby by setting EMBEDDED_RUBY=true in /etc/default/sensu'
+  s.post_install_message   = 'This plugin is meant to be run as asset from bonsai'
   s.require_paths          = ['lib']
   s.required_ruby_version  = '>= 2.3.0'
   s.summary                = 'Sensu plugins patroni monitoring'
   s.test_files             = s.files.grep(%r{^(test|spec|features)/})
   s.version                = SensuPluginsPatroni::Version::VER_STRING
 
-  s.add_runtime_dependency 'aws-sdk', '~> 2.3'
-  s.add_runtime_dependency 'oj', '~> 3.10'
-  s.add_runtime_dependency 'rest-client', '~> 2.1'
-  s.add_runtime_dependency 'sensu-plugin', '~> 4.0'
-  s.add_runtime_dependency 'typhoeus', '>= 1.3.1', '< 1.5.0'
+  s.add_runtime_dependency 'json',                       '~> 2.3'
+  s.add_runtime_dependency 'rest-client',                '~> 2.1'
+  s.add_runtime_dependency 'sensu-plugin',               '~> 4.0'
+  s.add_runtime_dependency 'typhoeus',                   '>= 1.3.1', '< 1.5.0'
 
-  s.add_development_dependency 'bundler',                   '~> 2.1'
-  s.add_development_dependency 'github-markup',             '~> 3.0'
-  s.add_development_dependency 'json',                      '~> 2.3'
-  s.add_development_dependency 'kitchen-docker',            '~> 2.6'
-  s.add_development_dependency 'kitchen-localhost',         '~> 0.3'
-  s.add_development_dependency 'kitchen-vagrant',           '~> 1.3'
-  s.add_development_dependency 'pry',                       '~> 0.10'
-  s.add_development_dependency 'rake',                      '~> 13.0'
+  s.add_development_dependency 'bundler',                '~> 2.1'
+  s.add_development_dependency 'github-markup',          '~> 3.0'
+  s.add_development_dependency 'json',                   '~> 2.3'
+  s.add_development_dependency 'pry',                    '~> 0.10'
+  s.add_development_dependency 'rake',                   '~> 13.0'
   s.add_development_dependency 'rdoc'
-  s.add_development_dependency 'redcarpet',                 '~> 3.2'
-  s.add_development_dependency 'rspec',                     '~> 3.1'
-  s.add_development_dependency 'rubocop',                   '~> 0.79.0'
-  s.add_development_dependency 'test-kitchen',              '~> 1.25.0'
-  s.add_development_dependency 'yard',                      '~> 0.9.11'
+  s.add_development_dependency 'redcarpet',              '~> 3.2'
+  s.add_development_dependency 'rspec',                  '~> 3.1'
+  s.add_development_dependency 'rubocop',                '~> 0.79.0'
+  s.add_development_dependency 'yard',                   '~> 0.9.11'
 end
